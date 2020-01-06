a38a126c1a217d4d5924f03ba29badb20602e8e2
SAPHemlock <55301948+SAPHemlock@users.noreply.github.com>
2020-01-06 12:36:29 -0800

    Added a JSX component properties.

--- React.js Essential Training/helloreact/src/index.js
+++ React.js Essential Training/helloreact/src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
 class Message extends React.Component {
-    render(){
-        return (
-            <div>
-                <h1>Hello Everyone</h1>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <h1 style={{ color: this.props.color }}>{this.props.msg}</h1>
+    <p>I'll check back in {this.props.minutes} minutes</p>
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(
-    <Message/>,    
-    document.getElementById('root')
-)
+  <Message msg="how are you?" color="blue" minutes={13}/>,
+  document.getElementById("root")
+);

2fda85cdfdf10eb8e0f07dd69e6ebf16ad72f516
SAPHemlock <55301948+SAPHemlock@users.noreply.github.com>
2020-01-06 11:00:54 -0800

    Create a react component using JSX.

--- React.js Essential Training/helloreact/src/index.js
+++ React.js Essential Training/helloreact/src/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-var style = {
-    backgroundColor: 'orange',
-    color: 'white',
-    fontFamily: 'Arial'
+class Message extends React.Component {
+    render(){
+        return (
+            <div>
+                <h1>Hello Everyone</h1>
+            </div>
+        )
+    }
 }
 
 ReactDOM.render(
-    <div style={style}>        
-        <h1 id='heading-element'>Hello World!</h1>    
-        <p>Good start</p>
-    </div>,    
+    <Message/>,    
     document.getElementById('root')
 )

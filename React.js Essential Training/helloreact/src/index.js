57946b06bd1b8f4533277512b1ee5ae8f8dc2d00
SAPHemlock <55301948+SAPHemlock@users.noreply.github.com>
2020-01-06 12:42:27 -0800

    Component created.

--- React.js Essential Training/helloreact/src/index.js
+++ React.js Essential Training/helloreact/src/index.js
@@ -1,18 +1,41 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-class Message extends React.Component {
+let skiData = {
+  total: 50,
+  powder: 20,
+  backcountry: 10,
+  goal: 100
+};
+
+class SkiDayCounter extends React.Component {
   render() {
     return (
-      <div>
-        <h1 style={{ color: this.props.color }}>{this.props.msg}</h1>
-    <p>I'll check back in {this.props.minutes} minutes</p>
-      </div>
+      <section>
+        <h3>Ski Days</h3>
+        <div>
+          <p>Total Days : {this.props.total}</p>
+        </div>
+        <div>
+          <p>Powder Days : {this.props.powder}</p>
+        </div>
+        <div>
+          <p>Backcountry Days : {this.props.backcountry}</p>
+        </div>
+        <div>
+          <p>Goal : {this.props.goal}</p>
+        </div>
+      </section>
     );
   }
 }
 
 ReactDOM.render(
-  <Message msg="how are you?" color="blue" minutes={13}/>,
+  <SkiDayCounter
+    total={skiData.total}
+    powder={skiData.powder}
+    backcountry={skiData.backcountry}
+    goal={skiData.goal}
+  />,
   document.getElementById("root")
 );

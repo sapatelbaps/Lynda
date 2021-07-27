9eedc5bba23d3ceffff13596516a118359a48467
Sandip <sapatelbaps@gmail.com>
2021-07-27 06:34:06 -0700

    Added C# test project with Red - fail scenario.

---
+++ C# Unit Testing/SalaryCalcTestProj/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SalaryCalcTestProj")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SalaryCalcTestProj")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e3f8f058-b055-4954-9a02-990abb549eb6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ C# Unit Testing/SalaryCalcTestProj/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace SalaryCalcTestProj
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void AnnualSalaryTest()
+        {
+            // Arrange
+            SalaryCalculator sc = new SalaryCalculator();
+
+            // Act
+            decimal annualSalary = sc.GetAnnualSalary();
+
+            // Assert
+            Assert.AreEqual(120000, annualSalary);
+
+        }
+    }
+}
